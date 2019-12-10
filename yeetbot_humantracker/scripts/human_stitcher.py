95f3e765308990b972d0537428ca95b7bd691762
Nickick-ICRS <nrh16@ic.ac.uk>
2019-12-10 16:24:53 +0000

    Human stitching works (totally thoroughly tested TM)

--- yeetbot_humantracker/scripts/human_stitcher.py
+++ yeetbot_humantracker/scripts/human_stitcher.py
@@ -6,8 +6,9 @@ from math import sqrt
 import rospy
 import tf
 
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, PoseStamped
 from yeetbot_msgs.msg import YEETBotHumanPoseArray
+from visualization_msgs.msg import Marker
 
 
 class Human:
@@ -23,6 +24,32 @@ class Human:
         # all poses are in the frame of the key used
         self.ids = {}
         self.poses = {}
+        self.pose_stamps = {}
+        self.updated_stamp = rospy.Time(0)
+    def calculate_pose(self):
+        dur = self.stamp - self.updated_stamp
+        if dur < rospy.Duration(0.02):
+            return
+        self.updated_stamp = self.stamp
+        pose = Pose()
+        div = 0
+        for key in self.poses:
+            p = self.poses[key]
+            dur = self.stamp - self.pose_stamps[key]
+            d = (1 + dur.secs + dur.nsecs * 1e-9)
+            pose.position.x += p.position.x / d
+            pose.position.y += p.position.y / d
+            div += 1/d
+        pose.position.x /= div
+        pose.position.y /= div
+        
+        pose.position.x *= 0.8
+        pose.position.x += 0.2 * self.pose.position.x
+        pose.position.y *= 0.8
+        pose.position.y += 0.2 * self.pose.position.y
+
+        pose.orientation.w = 1
+        self.pose = pose
 
 class HumanStitcher:
     def __init__(self):
@@ -57,18 +84,26 @@ class HumanStitcher:
         self.listener = tf.TransformListener()
 
         rospy.Subscriber(
-            '/door_tracker', YEETBotHumanPoseArray, self.human_pose_cb)
+            '/humandetect_door_poses', YEETBotHumanPoseArray,
+            self.human_pose_cb)
         rospy.Subscriber(
-            '/window_tracker', YEETBotHumanPoseArray, self.human_pose_cb)
+            '/humandetect_window_poses', YEETBotHumanPoseArray,
+            self.human_pose_cb)
         self.pub = rospy.Publisher(
             '/stitched_human_poses', YEETBotHumanPoseArray, queue_size=2)
+        self.rviz_pub = rospy.Publisher(
+            '/human_pose_visualization', Marker, queue_size=1)
 
 
     def human_pose_cb(self, pose_array):
         with self.lock:
             new_arr = []
-            for pose in human_poses.poses:
-                new_arr.append(self.listener.transformPose('map', pose))
+            for pose in pose_array.human_poses.poses:
+                ps = PoseStamped()
+                ps.pose = pose
+                ps.header = pose_array.header
+                ps = self.listener.transformPose('map', ps)
+                new_arr.append(ps.pose)
             pose_array.human_poses.poses = new_arr
             pose_array.human_poses.header.frame_id = 'map'
             self.pose_arrays[pose_array.header.frame_id] = pose_array.human_poses
@@ -77,7 +112,10 @@ class HumanStitcher:
 
     def run(self):
         while not rospy.is_shutdown() and len(self.pose_arrays) < 2:
-            rospy.sleep(0.1)
+            msg = "Waiting for pose arrays... Currently have " 
+            for key in self.pose_arrays:
+                msg += key + " " 
+            rospy.sleep(1)
 
         # Run at 10 Hz
         rate = rospy.Rate(10)
@@ -98,11 +136,11 @@ class HumanStitcher:
                         update_keys.append(key)
 
                 # Stitch together info and estimate poses of humans
-                stitch_frames(update_keys)
+                self.stitch_frames(update_keys)
                 # Delete humans that haven't been updated for a while
-                kill_humans()
+                self.kill_humans()
                 # Publish the current list of humans
-                publish_humans()
+                self.publish_humans()
 
             rate.sleep()
 
@@ -124,8 +162,9 @@ class HumanStitcher:
             for key in keys:
                 human_closest_poses[human][key] = []
                 human_closest_ids[human][key] = []
-                for pose, id_ in zip(self.poses[key].poses, self.ids[key]):
-                    if len(human_closest_poses[human][key] < 3):
+                for pose, id_ in zip(self.pose_arrays[key].poses,
+                                     self.id_arrays[key]):
+                    if len(human_closest_poses[human][key]) < 3:
                         human_closest_poses[human][key].append(pose)
                         human_closest_ids[human][key].append(id_)
                     else:
@@ -150,7 +189,7 @@ class HumanStitcher:
                     continue
                 id_ = human.ids[key]
                 reserved_ids[key] = []
-                for (i, p) in zip(self.ids[key], self.poses[key]):
+                for (i, p) in zip(self.id_arrays[key], self.pose_arrays[key].poses):
                     if i == id_:
                         reserved_ids[key].append(id_)
                         if id_ not in human_closest_ids[human][key]:
@@ -173,14 +212,14 @@ class HumanStitcher:
         for key in keys:
             used_ids[key] = []
             unused_ids[key] = []
-            for id_ in self.ids[key]:
+            for id_ in self.id_arrays[key]:
                 unused_ids[key].append(id_)
 
         for human in self.humans:
             # Pick the closest point that hasn't been used for each key
             poses = dict()
             ids = dict()
-            for key in key:
+            for key in keys:
                 poses[key] = None
                 ids[key] = None
                 max_dist = 9e9
@@ -188,16 +227,23 @@ class HumanStitcher:
                                        human_closest_ids[human][key]):
                     # If the ID is reserved and equal to an existing ID
                     # then that ID will always be this human
-                    if id_ == human.ids[key] and id_ in reserved_ids[key] and id_ not in used_ids[key]:
-                        poses[key] = pose
-                        ids[key] = id_
-                        break
+                    if key in human.ids:
+                        if id_ == human.ids[key] and id_ in reserved_ids[key] and id_ not in used_ids[key]:
+                            poses[key] = pose
+                            ids[key] = id_
+                            break
 
                     dist = self.dist(pose, human.pose)
-                    if dist < max_dist and id_ not in used_ids[key] and id_ not in reserved_ids[key] and dist < self.MAX_HUMAN_DELTA:
-                        max_dist = dist
-                        poses[key] = pose
-                        ids[key] = id_
+                    if dist < max_dist and dist < self.MAX_HUMAN_DELTA:
+                        if id_ not in used_ids[key]:
+                            if key not in reserved_ids:
+                                max_dist = dist
+                                poses[key] = pose
+                                ids[key] = id_
+                            elif id_ not in reserved_ids[key]:
+                                max_dist = dist
+                                poses[key] = pose
+                                ids[key] = id_
                 if ids[key] == None:
                     # This human has no matching points in this key
                     pass
@@ -208,32 +254,46 @@ class HumanStitcher:
                     human.poses[key] = poses[key]
                     human.ids[key] = ids[key]
                     human.stamp = stamp
+                    human.pose_stamps[key] = stamp
 
         # Now try and match the remaining points and create new humans
         # dict of [key]: dict[id]
         matching_pairs = dict()
         for key in keys:
+            matching_pairs[key] = dict()
             # dict of [id]: ((point, point2, id2, key2), dist)
             for key2 in keys:
                 if key == key2:
                     continue
-                for (p, i) in zip(human_closest_poses[human][key],
-                                  human_closest_ids[human][key]):
-                    if i in used_ids:
+                for (p, i) in zip(self.pose_arrays[key].poses,
+                                  self.id_arrays[key]):
+                    if i in used_ids[key]:
                         continue
-                    matching_pairs[key][i] = ((Pose(), Pose(), -1, key2), 
-                                              self.MAX_DELTA)
-                    for (p2, i2) in zip(human_closest_poses[human][key2],
-                                        human_closest_ids[human][key2]):
-                        if i2 in used_ids:
+                    for (p2, i2) in zip(self.pose_arrays[key2].poses,
+                                        self.id_arrays[key2]):
+                        if i2 in used_ids[key2]:
                             continue
                         dist = self.dist(p, p2)
-                        if dist < min_dist[key][i][1]:
-                            matching_pairs[key][i][0] = (p, p2, i2, key2)
-                            matching_pairs[key][i][1] = dist
+                        if (i not in matching_pairs[key] and dist < self.MAX_DELTA):
+                            matching_pairs[key][i] = []
+                            matching_pairs[key][i].append((p, p2, i2, key2))
+                            matching_pairs[key][i].append(dist)
+                        elif i in matching_pairs[key]:
+                            if dist < matching_pairs[key][i][1]:
+                                matching_pairs[key][i][0] = (p, p2, i2, key2)
+                                matching_pairs[key][i][1] = dist
             # Sort the dict such that the closest pairs are together
-            matching_pairs[key] = sorted(
-                matching_pairs[key], key=lambda x: x[1])
+            try:
+                def sort_func(id_):
+                    return matching_pairs[key][id_][1]
+                res = sorted(
+                    matching_pairs[key], key=sort_func)
+                new_dict = dict()
+                for id_ in res:
+                    new_dict[id_] = matching_pairs[key][id_]
+                matching_pairs[key] = new_dict
+            except KeyError:
+                matching_pairs[key] = {}
 
         # Now create a list containing [(key, id, point), ...]
         sets_of_points = []
@@ -242,9 +302,21 @@ class HumanStitcher:
         # to that set if it does. If both match an existing id then we
         # discard the points. If none match an existing id then we create a
         # new set.
-        for key in keys:
+
+        ids_to_del = []
+        for key in matching_pairs:
+            for id_ in matching_pairs[key]:
+                if len(matching_pairs[key][id_]) == 0:
+                    ids_to_del.append((key, id_))
+        for (key, id_) in ids_to_del:
+            del matching_pairs[key][id_]
+        
+        keys_to_del = []
+        for key in matching_pairs:
             if len(matching_pairs[key]) == 0:
-                del matching_pairs[key]
+                keys_to_del.append(key)
+        for key in keys_to_del:
+            del matching_pairs[key]
 
         while len(matching_pairs) != 0:
             # Will be ((point, point2, id2, key2), dist)
@@ -254,11 +326,12 @@ class HumanStitcher:
             for key in matching_pairs:
                 for id_ in matching_pairs[key]:
                     best_pair = matching_pairs[key][id_]
+                    best_id = id_
+                    best_key = key
                     break
-                break
             
             for key in matching_pairs:
-                for id_ in matching_pairs[id_]:
+                for id_ in matching_pairs[key]:
                     pair = matching_pairs[key][id_][0]
                     if pair[1] < best_pair[1]:
                         best_pair = pair
@@ -304,37 +377,65 @@ class HumanStitcher:
                 used_ids[best_pair[0][3]].append(best_pair[0][2])
                 unused_ids[best_pair[0][3]].remove(best_pair[0][2])
 
+            ids_to_del = []
             for key in matching_pairs:
                 for id_ in matching_pairs[key]:
                     if len(matching_pairs[key][id_]) == 0:
-                        del matching_pairs[key][id_]
+                        ids_to_del.append((key, id_))
+            for (key, id_) in ids_to_del:
+                del matching_pairs[key][id_]
+            
+            keys_to_del = []
+            for key in matching_pairs:
                 if len(matching_pairs[key]) == 0:
-                    del matching_pairs[key]
+                    keys_to_del.append(key)
+            for key in keys_to_del:
+                del matching_pairs[key]
 
         # Now we have a set of points which have been grouped together - 
         # these are our new humans.
 
-        for point in set_of_points:
+        for point in sets_of_points:
             h = Human(self.id_counter)
-            id_counter += 1
+            self.id_counter += 1
             h.stamp = stamp
             for i in range(len(point)):
                 h.ids[point[i][0]] = point[i][1]
                 h.poses[point[i][0]] = point[i][2]
-                h.pose.position.x += point[i][2].position.x
-                h.pose.position.y += point[i][2].position.y
-            h.pose.position.x /= len(point)
-            h.pose.position.y /= len(point)
-
-            # We don't care what orientation humans are at
-            h.pose.orientation.w = 1
+                h.pose_stamps[point[i][0]] = stamp
             self.humans.append(h)
 
         # The points not being used are visible only on one camera _and_ 
-        # have not yet been matched with an existing human. As such there is
-        # no way to know if they are noise or really humans.
-        print used_ids
-        print unused_ids
+        # have not yet been matched with an existing human. If there are no
+        # humans near them then we can make a new human out of them
+
+        for key in keys:
+            for id_ in unused_ids[key]:
+                unique = True
+                it = 0
+                for id2 in self.id_arrays[key]:
+                    if id2 == id_:
+                        break
+                    it += 1
+
+                pose = self.pose_arrays[key].poses[it]
+                for human in self.humans:
+                    human.calculate_pose()
+                    if self.dist(human.pose, pose) < self.MAX_DELTA:
+                        unique = False
+
+                # If there weren't any humans near this point then we can 
+                # count it as a human
+                if unique:
+                    h = Human(self.id_counter)
+                    self.id_counter += 1
+                    h.stamp = stamp
+                    h.ids[key] = id_
+                    h.poses[key] = pose
+                    h.pose_stamps[key] = stamp
+                    self.humans.append(h)
+                used_ids[key].append(id_)
+                unused_ids[key].remove(id_)
 
     def dist(self, pose1, pose2):
         dx = pose1.position.x - pose2.position.x
@@ -352,9 +453,39 @@ class HumanStitcher:
         msg.header.stamp = rospy.Time.now()
         msg.human_poses.header = msg.header
         for h in self.humans:
+            h.calculate_pose()
             msg.human_poses.poses.append(h.pose)
             msg.ids.append(h.id)
+        rospy.logwarn(msg)
+        self.visualise(msg)
         self.pub.publish(msg)
+
+    def visualise(self, pose_array):
+        marker = Marker()
+        marker.header = pose_array.header
+        marker.ns = 'humans'
+        
+        marker.type = Marker.ARROW
+
+        marker.pose.orientation.y = -0.7071
+        marker.pose.orientation.w = 0.7071
+
+        marker.scale.x = 2
+        marker.scale.y = 0.4
+        marker.scale.z = 0.4
+
+        marker.color.r = 0.5
+        marker.color.g = 0.0
+        marker.color.b = 1.0
+        marker.color.a = 0.6
+
+        marker.lifetime = rospy.Duration(3)
+
+        for (pose, id_) in zip(pose_array.human_poses.poses, 
+                               pose_array.ids):
+            marker.id = id_
+            marker.pose.position = pose.position
+            self.rviz_pub.publish(marker)
 
 def main():
     try:
